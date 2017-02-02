2d76eb07cdc22f1a2c9eb5c05b0cb8c996ff09ab
Dridk <dridk@163-172-73-24.online.net>
2017-02-02 10:49:20 +0100

    add conda support

--- database/download_greengene.sh
+++ database/download_greengene.sh
@@ -1,2 +1,4 @@
 wget ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_5_taxonomy.txt.gz
 wget ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_5.fasta.gz
+
+wget ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_5_taxonomy.txt.gz
